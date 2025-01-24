e95017fa2105cf6cd04f7429200c5011e7daa2a4
novern <novner@sina.cn>
2025-01-24 23:41:59 +0800

    Update Code

--- Case Study/Figure10.m
+++ Case Study/Figure10.m
@@ -1,8 +1,7 @@
 close all;
-% Data are publicly available at https://biaowang.tech/xjtu-sy-bearing-datasets
 folderPath = '../Data/Bearing1_3';
 addpath('../routine'); 
-addpath(folderPath); 
+addpath('../Data/Bearing1_3'); % Data are publicly available at https://biaowang.tech/xjtu-sy-bearing-datasets
 rng('default')
 
 sigma = 1; 
@@ -12,14 +11,10 @@ Periods = 3277:-1:2;
 P = length(Periods);
 len = 32768;
 
-% 获取文件夹中的CSV文件数量以确定num
-
-files = dir(fullfile(folderPath, '*.csv'));
-num = length(files);  % 动态确定epoch数量
-mod = 0;  % 根据需要修改此值
-training_epoch = 20;  % 确保training_epoch不超过可用的文件数量
+num = 158;  % number of epochs
+mod = 0;
+training_epoch = 20;
 testing_epoch = num - mod;
-
 
 % Searching range for MO statistic
 if ~exist('C', 'var')
@@ -27,15 +22,14 @@ if ~exist('C', 'var')
     L = chol(C, 'lower');
 end
 
-
-% 初始化统计量
+% Initiate Statistics
 t2stat = zeros(training_epoch, 1);
 maxstat = zeros(training_epoch, 1);
 fstat = zeros(training_epoch, 1);
 
-% 读取数据并计算Phase 1统计量
 for i = 1:training_epoch
-    filename = fullfile(folderPath, [num2str(i + mod) '.csv']);
+    filename = [num2str(i + mod) '.csv'];
+    
     [data] = csvread(filename,1,0);
     signal = data(:,1).^2 + data(:,2).^2;
     signal = zscore(signal);
@@ -45,11 +39,11 @@ for i = 1:training_epoch
     [fstat(i),~] = Fchart(signal);
 end
 
-% 计算均值和标准差
+% sample mean and std
 mu_t2 = mean(t2stat);
 sigma_t2 = std(t2stat);
 mu_max = mean(maxstat);
-disp(mu_max);
+
 sigma_max = std(maxstat);
 mu_f = mean(fstat);
 sigma_f = std(fstat);
@@ -67,12 +61,12 @@ upper_limit_f = zeros(testing_epoch, 1);
 ewma_stat_f = zeros(testing_epoch, 1);
 
 for i = 1:testing_epoch
-    filename = fullfile(folderPath, [num2str(i + mod) '.csv']);
-    [data] = xlsread(filename);
+    filename = [num2str(i + mod) '.csv'];
+    [data] = csvread(filename,1,0);
     signal = data(:,1).^2 + data(:,2).^2;
     signal = zscore(signal);
     
-    % 计算控制限和EWMA统计量
+    % control limit
     tmp_t2 = L_para * sigma_t2 * sqrt(lambda/(2-lambda)*(1-(1-lambda)^(2*i)));
     upper_limit_t2(i) = mu_t2 + 2 * tmp_t2;
     
@@ -86,7 +80,7 @@ for i = 1:testing_epoch
     [test_stat_max, ~, ~] = Max_NRC(signal, L, Periods);
     [test_stat_f, ~] = Fchart(signal);
     
-    % 计算EWMA统计量
+    % compute EWMA statistics
     if i == 1
         ewma_stat_t2(i) = lambda * test_stat_t2 + (1 - lambda) * mu_t2;
         ewma_stat_max(i) = lambda * test_stat_max + (1 - lambda) * mu_max;
@@ -98,7 +92,7 @@ for i = 1:testing_epoch
     end
 end
 
-% 绘制并保存控制图
+% plot
 figure;
 plot((1:testing_epoch)', ewma_stat_max,'Color',[0.85,0.33,0.10]);
 hold on;
